a3a84d213c2cfc4095d0b4794f8d794a7f2f0d76
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-04-16 19:05:13 -0400

    Unified DiscordBot class structure

--- src/main/java/PDA/DiscordBot.java
+++ src/main/java/PDA/DiscordBot.java
@@ -2,41 +2,44 @@ package PDA;
 
 // import PDA.DiscordBotJoin;
 
-import PDA.PostCard;
-import PDA.EventListener;
-import PDA.PDA;
+import ch.qos.logback.classic.Logger;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Channel;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.GuildChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 import java.awt.*;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Implementation of the JDA discord bot API.
- *
+ * <p>
  * Responsibilities:
- *
+ * <p>
  * 1) Initialize the discord bot with a given token
  * 2) Initialize containers for embeds, text channels, and posts per discord server
  * 3) Provide a layer of abstraction between the rest of the program and the JDA discord bot API to allow for sending messages and embeds to a server
- *
  */
 
 public class DiscordBot {
 	private JDA jda;
-	private HashMap<Guild, EmbedBuilder> embedMap;
-	private HashMap<Guild, TextChannel> channels;
+	private final HashMap<Guild, EmbedBuilder> embedMap;
+	private final HashMap<Guild, TextChannel> channels;
+	public final Logger log;
 
 	public DiscordBot(String token) throws LoginException, InterruptedException {
-		embedMap = new HashMap<>();
-		channels = new HashMap<>();
+		this.log = (Logger) LoggerFactory.getLogger(this.getClass().getName());
+		this.log.info("Initializing Discord Bot...");
+
+		this.embedMap = new HashMap<>();
+		this.channels = new HashMap<>();
 
 		//! setup JDA bot
 		setupJDA(token);
@@ -50,11 +53,11 @@ public class DiscordBot {
 		//! setup the container for private and public posts so it's not null
 		setupPosts();
 
-		System.out.println("channels: " + channels);
+		this.log.info("Servers containing PDA: " + channels);
 	}
 
 	public void setTitle(String title, String url, Guild id) {
-		embedMap.put(id, embedMap.get(id).setTitle(title, url));
+		this.embedMap.put(id, this.embedMap.get(id).setTitle(title, url));
 	}
 
 	public void addChannel(String channelId, Guild id) {
@@ -62,43 +65,43 @@ public class DiscordBot {
 		// JDA has another overloaded getTextChannelById() method that uses a long instead of a String
 		// for some reason it will print a NumberFormatException error when using the String version even though it works
 		try {
-			channels.put(id, jda.getTextChannelById(channelId));
+			this.channels.put(id, jda.getTextChannelById(channelId));
 		} catch (NumberFormatException ignored) {
 		}
 	}
 
 	public void setDescription(String description, Guild id) {
-		embedMap.put(id, embedMap.get(id).setDescription(description));
+		this.embedMap.put(id, this.embedMap.get(id).setDescription(description));
 	}
 
 	public void addField(String title, String value, Guild id) {
-		embedMap.put(id, embedMap.get(id).addField(title, value, true));
+		this.embedMap.put(id, this.embedMap.get(id).addField(title, value, true));
 	}
 
 	public void setColor(Color color, Guild id) {
-		embedMap.put(id, embedMap.get(id).setColor(color));
+		this.embedMap.put(id, this.embedMap.get(id).setColor(color));
 	}
 
 	public void setFooter(String text, String userUrl, Guild id) {
-		embedMap.put(id, embedMap.get(id).setFooter(text, userUrl));
+		this.embedMap.put(id, this.embedMap.get(id).setFooter(text, userUrl));
 	}
 
 	public void send(Guild id) { // sending embed
-		channels.get(id).sendMessageEmbeds(embedMap.get(id).build()).queue();
-		embedMap.get(id).clear();
+		this.channels.get(id).sendMessageEmbeds(this.embedMap.get(id).build()).queue();
+		this.embedMap.get(id).clear();
 	}
 
 	public void send(String text, Guild id) { // sending text
-		channels.get(id).sendMessage(text).queue();
+		this.channels.get(id).sendMessage(text).queue();
 	}
 
 	public void addGuild(Guild id) {
-		embedMap.put(id, embedMap.getOrDefault(id, new EmbedBuilder()));
+		this.embedMap.put(id, this.embedMap.getOrDefault(id, new EmbedBuilder()));
 	}
 
 	// function to help with testing
 	public Set<Guild> getAllGuilds() {
-		return embedMap.keySet();
+		return this.embedMap.keySet();
 	}
 
 	// function to help with testing
@@ -108,18 +111,19 @@ public class DiscordBot {
 
 	private void setupJDA(String token) throws InterruptedException {
 		try {
-			jda = JDABuilder.createDefault(token).build();
+			this.jda = JDABuilder.createDefault(token).build();
+			this.jda.getPresence().setActivity(Activity.playing("Type /help"));
 		} catch (LoginException e) {
-			System.out.println("The given Discord Bot token '" + token + "' is invalid!");
+			this.log.error("The given Discord Bot token '{}' is invalid!", token);
 			System.exit(1);
 		}
-		jda.awaitReady();
-		jda.addEventListener(new EventListener(this));
+		this.jda.awaitReady();
+		this.jda.addEventListener(new EventListener(this));
 	}
 
 	private void setupEmbeds() {
-		for (Guild guild : jda.getGuilds()) {
-			if (!embedMap.containsKey(guild)) {
+		for (Guild guild : this.jda.getGuilds()) {
+			if (!this.embedMap.containsKey(guild)) {
 				addGuild(guild);
 				PDA.guildSet.add(guild);
 			}
@@ -128,24 +132,24 @@ public class DiscordBot {
 
 	private void setupTextChannels() {
 		for (Guild guild : PDA.guildSet) {
-			List<TextChannel> chanList = guild.getTextChannelsByName("testing", true);
+			List<TextChannel> chanList = new LinkedList<>(); //guild.getTextChannelsByName("testing", true);
 
 			// if we can't find a "testing" then output to the first channel we find
 			if (chanList.isEmpty()) {
 				chanList = guild.getTextChannels();
 			}
-			addChannel(chanList.get(0).getId(), guild);
+
+			if (!chanList.isEmpty())
+				addChannel(chanList.get(0).getId(), guild);
 		}
 	}
 
 	private void setupPosts() {
-
 		for (Guild guild : PDA.guildSet) {
 			LinkedList<PostCard> temp = new LinkedList<>();
 
 			// TODO: when we save the posts we will take it from there instead
 			PDA.postCards.put(guild, temp);
-
 		}
 	}
 }
