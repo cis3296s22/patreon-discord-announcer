4a3219517981ff4affca2499392debc993ccd19b
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-04-16 23:54:24 -0400

    Added JSONHelper class -JSONHelper will assist in loading saved post cards in JSON format

---
+++ src/main/java/PDA/JSONHelper.java
@@ -0,0 +1,80 @@
+package PDA;
+
+import ch.qos.logback.classic.Logger;
+import net.dv8tion.jda.api.entities.Guild;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class JSONHelper {
+	private static final Logger log = (Logger) LoggerFactory.getLogger("JSONHelper");
+
+	public static JSONObject parseJSONFile(String filename) {
+		// Container for the JSON text we will attempt to read.
+		String jsonText;
+
+		// Set jsonText to filenames text if the file exists, otherwise return null.
+		try {
+			jsonText = IOUtils.toString(new FileInputStream(filename), StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			return null;
+		}
+
+		return new JSONObject(jsonText);
+	}
+
+	public static JSONObject createPostJSONObject(PostCard postCard) {
+		JSONObject postDetails = new JSONObject();
+
+		postDetails.put("isPrivate", postCard.isPrivate());
+		postDetails.put("publishDate", postCard.getPublishDate());
+		postDetails.put("title", postCard.getTitle());
+		postDetails.put("content", postCard.getContent());
+
+		return postDetails;
+	}
+
+	public static void parseSavedData(DiscordBot bot, String fileName) {
+		JSONObject savedJson = JSONHelper.parseJSONFile(fileName);
+
+		if (savedJson == null) {
+			log.error("An error occurred while reading '" + fileName + "'");
+			System.exit(1);
+		}
+
+		// Loop through every URL stored
+		for (String curPatreonUrl : savedJson.keySet()) {
+			JSONObject curWebsite = savedJson.getJSONObject(curPatreonUrl);
+
+			// Loop through every guild ID stored inside this URL
+			for (String curGuildId : curWebsite.keySet()) {
+				JSONObject guildPosts = curWebsite.getJSONObject(curGuildId);
+
+				for (String curPostUrl : guildPosts.keySet()) {
+					JSONObject postDetails = (JSONObject) guildPosts.get(curPostUrl);
+
+					String publishDate, title, content;
+					boolean isPrivate;
+
+					try {
+						publishDate = postDetails.get("publishDate").toString();
+						title = postDetails.get("title").toString();
+						content = postDetails.get("content").toString();
+						isPrivate = Boolean.getBoolean(postDetails.get("isPrivate").toString());
+
+						PostCard postCard = new PostCard(publishDate, title, curPostUrl, content, isPrivate);
+						Guild guild = bot.getJDA().getGuildById(curGuildId);
+
+						PDA.addPostCard(postCard, guild);
+					} catch (Exception e) {
+						log.error("An error occurred while getting post '{}' for guild '{}'", curPostUrl, curGuildId);
+					}
+				}
+			}
+		}
+	}
+}
